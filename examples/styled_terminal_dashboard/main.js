13e72c136ecd1c729713ebb1f0b326de3246e926
Miguel Mesquita <mm@cob.pt>
2021-03-25 15:50:12 +0000

    fix: upgrade examples

--- examples/styled_terminal_dashboard/main.js
+++ examples/styled_terminal_dashboard/main.js
@@ -1,21 +1,26 @@
-const { auth, DefinitionCount } = require("@cob/dashboard-info")
-
 const logUpdate = require('log-update')
 const chalk = require('chalk');
 const CFonts = require('cfonts');
-const terminalLink = require('terminal-link')
+const terminalLink = require('terminal-link');
 
-function showCB(value, resultsUrl) {
+function showCB(updateInfo) {
+    let value = updateInfo.value
+    let href = updateInfo.href
     logUpdate(
         chalk.green.bold("Number of test persons:\n")
         + CFonts.render(" "+value, {colors: [value>6 ? 'red' : value > 4 ? 'yellow' : 'green', 'system']}).string
-        + (resultsUrl != '' ? ("\n     " + terminalLink(chalk.blue("(results)"), resultsUrl)) : "")
+        + (href != '' ? ("\n     " + terminalLink(chalk.blue("(results)"), href)) : "")
     )
 }
 
-async function start() {
-    await auth("jestTests", "1jestTests2")
-    new DefinitionCount("Test Person", showCB, 1 )
+async function main() {
+    const dashInfo = await import("@cob/dashboard-info")
+
+    dashInfo.setServer("https://learning.cultofbits.com")
+    await dashInfo.auth("jestTests", "1jestTests2")
+
+    let totalInfo = dashInfo.definitionCount("Test Person","*", {changeCB:showCB, validity:1})
+    showCB(totalInfo) // Show initial information
 }
 
-start()
+main()
