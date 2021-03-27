ac0622123d0ea145f275346d0c1268029e39da7a
Miguel Mesquita <mm@cob.pt>
2021-03-27 12:35:49 +0000

    refactor: better countInfo name

--- tests/DashInfo.test.js
+++ tests/DashInfo.test.js
@@ -14,59 +14,62 @@ test('every DashInfo value starts by beeing the last cached value', async () => 
     localStorage.setItem("anonymous-countCalls_Results",JSON.stringify({"value":42}));
 
     
-    let ai = new DashInfo( {validity:1}, newCountCalls())
-    expect(ai.value).toBe(42)
+    let countInfo = new DashInfo( {validity:1}, newCountCalls())
+    expect(countInfo.username).toBe("anonymous")
+    expect(countInfo.id).toBe("countCalls")
+    expect(countInfo.cacheId).toBe("anonymous-countCalls")
+    expect(countInfo.value).toBe(42)
 
     await Promise.resolve() //Allow other promises to resolve
-    expect(ai.value).toBe(1)
-    ai.stopUpdates()
+    expect(countInfo.value).toBe(1)
+    countInfo.stopUpdates()
 })
 
-test('DashInfo should only have a new value every *validity* seconds ',  async () => {
+test('DashInfo should only have countInfo new value every *validity* seconds ',  async () => {
     jest.useFakeTimers('modern')
     jest.setSystemTime(0)   
     
-    let a = new DashInfo( {validity:1}, newCountCalls(), 0)
-    expect(a.value).toBeUndefined()
+    let countInfo = new DashInfo( {validity:1}, newCountCalls(), 0)
+    expect(countInfo.value).toBeUndefined()
     
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(1)
+    expect(countInfo.value).toBe(1)
     
     jest.setSystemTime(101)
     jest.advanceTimersByTime(100)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(1) // Shouldn't change
+    expect(countInfo.value).toBe(1) // Shouldn't change
     
     jest.setSystemTime(801)
     jest.advanceTimersByTime(700)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(1) // Still shouldn't change
+    expect(countInfo.value).toBe(1) // Still shouldn't change
     
     
     jest.setSystemTime(1101)
     jest.advanceTimersByTime(300)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(2) // CHANGE TIME !
+    expect(countInfo.value).toBe(2) // CHANGE TIME !
     
     jest.setSystemTime(1601)
     jest.advanceTimersByTime(500)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(2) // Shouldn't change
+    expect(countInfo.value).toBe(2) // Shouldn't change
     
     jest.setSystemTime(2101)
     jest.advanceTimersByTime(400)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(3) // CHANGE TIME !
+    expect(countInfo.value).toBe(3) // CHANGE TIME !
     
     jest.setSystemTime(2601)
     jest.advanceTimersByTime(500)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(3) // Shouldn't change
+    expect(countInfo.value).toBe(3) // Shouldn't change
     
     jest.setSystemTime(3101)
     jest.advanceTimersByTime(500)
     await Promise.resolve() //Allow other promises to resolve
-    expect(a.value).toBe(4) // CHANGE TIME !
+    expect(countInfo.value).toBe(4) // CHANGE TIME !
 })    
 
 test('TWO objects for the same info should only call ONE _getNewValue() every *validity* seconds ', () => {
@@ -84,17 +87,17 @@ test('if no cache available (no mem or no localstorage) it work without cache', 
 test('changing querys for "countries series" from "Arab world" to "united" should change 20 to 60', () => {
 // TODO
 //     const mockUpdateCb = jest.fn()
-//     dc = new DefinitionCount("Countries Series", mockUpdateCb, 1, "Arab world", "c2" )
+//     countInfo = new DefinitionCount("Countries Series", mockUpdateCb, 1, "Arab world", "c2" )
     
 //     return sleep(500).then( () => {
-//         expect(dc.resultsUrl).toBe("https://learning.cultofbits.com/recordm/#/definitions/2/q=Arab world")
-//         expect(dc.getValue()).toBe(20)
+//         expect(countInfo.resultsUrl).toBe("https://learning.cultofbits.com/recordm/#/definitions/2/q=Arab world")
+//         expect(countInfo.getValue()).toBe(20)
 
-//         dc.setQuery("United")
+//         countInfo.setQuery("United")
 //         sleep(2500).then( () => {
-//             dc.stopUpdates()
-//             expect(dc.resultsUrl).toBe("https://learning.cultofbits.com/recordm/#/definitions/2/q=United")
-//             expect(dc.getValue()).toBe(60)
+//             countInfo.stopUpdates()
+//             expect(countInfo.resultsUrl).toBe("https://learning.cultofbits.com/recordm/#/definitions/2/q=United")
+//             expect(countInfo.getValue()).toBe(60)
 //             done()
 //         })
 //     })
