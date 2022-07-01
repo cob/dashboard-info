b67fe4e5423f610e7ef18853b84e3195e4999d81
jbarata <joao.barata@cultofbits.com>
2022-07-01 18:40:23 +0100

    feat:new option dmEquipmentSearch

---
+++ src/DmEquipmentCount.js
@@ -0,0 +1,13 @@
+import { dmEquipmentSearch } from "@cob/rest-api-wrapper"
+
+const equipmentCount = ({query}) =>
+  dmEquipmentSearch(query, 0, 0)
+  .then(response => 
+    ({
+      value: response.hits.total.value,
+      href: response.resultsUrl
+    })
+  )
+  .catch ( e => { throw(e) })
+
+export default equipmentCount
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import fSum     from "./FieldSum.js"
 import fAverage from "./FieldAverage.js"
 import fWeightedAverage from "./FieldWeightedAverage.js"
 import fValues  from "./FieldValues.js"
+import dmEquipCount  from "./DmEquipmentCount.js"
 
 
 const definitionCount = (definitionName,   query="*",          options={}) => new DashInfo(options, defCount, {query:query, definitionName:definitionName })
@@ -17,5 +18,6 @@ const fieldAverage    = (defId, fieldName, query="*",          options={}) => ne
 const fieldWeightedAverage = (defId, fieldName, weightFieldName, query="*", options={}) => new DashInfo(options, fWeightedAverage(), {query:query, defId:defId, fieldName:fieldName, weightFieldName:weightFieldName })
 const instancesList   = (definitionName,   query="*", size=10, start=0, options={}) => new DashInfo(options, instList, {query:query, definitionName:definitionName, size:size, start:start })
 const fieldValues     = (defId, fieldName, query="*", size=10, options={}) => new DashInfo(options, fValues,  {query:query, defId:defId, fieldName:fieldName, size:size })
+const dmEquipmentCount = (query="*", options={}) => new DashInfo(options, dmEquipCount(),  {query:query})
 
-export { DashInfo, definitionCount, instancesList, domainCount, fieldSum, fieldAverage, fieldWeightedAverage, fieldValues, setServer, auth, umLoggedin }
+export { DashInfo, definitionCount, instancesList, domainCount, fieldSum, fieldAverage, fieldWeightedAverage, fieldValues, setServer, auth, umLoggedin, dmEquipmentCount }
