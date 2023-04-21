df79036d52ea56dc51e532d7fb9555f20f230280
Miguel Mesquita <mm@cob.pt>
2023-04-21 11:25:41 +0100

    fix: remove misleading warning

--- src/index.js
+++ src/index.js
@@ -16,15 +16,7 @@ const domainCount     = (domainId,         query="*",          options={}) => ne
 const fieldSum        = (defId, fieldName, query="*",          options={}) => new DashInfo(options, fSum,     {query:query, defId:defId, fieldName:fieldName })
 const fieldAverage    = (defId, fieldName, query="*",          options={}) => new DashInfo(options, fAverage, {query:query, defId:defId, fieldName:fieldName })
 const fieldWeightedAverage = (defId, fieldName, weightFieldName, query="*", options={}) => new DashInfo(options, fWeightedAverage(), {query:query, defId:defId, fieldName:fieldName, weightFieldName:weightFieldName })
-const instancesList   = function(definitionName,   query="*", size=10, start=0, sort="", ascending="", options={}){
-    //note: cannot be an arrow function because Arrow functions don't have their own bindings to 'arguments'
-    if (arguments.length < 7) {
-        console.warn("Number of arguments calling `instancesList` is less than the expected 7. Maybe the call was written for an older version of dashinfo.\n"
-                     + "Make sure you have no holes in args list (pass undefined in those args if needed).", arguments)
-    }
-
-    return new DashInfo(options, instList, {query:query, definitionName:definitionName, size:size, start:start, sort:sort, ascending:ascending })
-}
+const instancesList   = (definitionName,   query="*", size=10, start=0, sort="", ascending="", options={}) => new DashInfo(options, instList, {query:query, definitionName:definitionName, size:size, start:start, sort:sort, ascending:ascending })
 const fieldValues     = (defId, fieldName, query="*", size=10, options={}) => new DashInfo(options, fValues,  {query:query, defId:defId, fieldName:fieldName, size:size })
 const dmEquipmentCount = (query="*", options={}) => new DashInfo(options, dmEquipCount, {query:query})
 
