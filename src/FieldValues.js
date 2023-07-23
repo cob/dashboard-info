3d0bcf9b735995ee60f7a5cca0fbf946853d1882
Miguel Mesquita <mm@cob.pt>
2023-07-23 18:31:54 +0100

    feat: allow sort of distinct function

--- src/FieldValues.js
+++ src/FieldValues.js
@@ -14,18 +14,26 @@ const fieldValues = ({def, fieldName, query, size=10, sort="", ascending="asc" }
     }
   }
 
-  return rmDefinitionAggregation(def, agg , query, 0, size, sort="", ascending="asc")
+  return rmDefinitionAggregation(def, agg , query, 0, size, sort=sort, ascending="asc")
     .then(response => {
       let keysCount = response.aggregations['sterms#x'].buckets
       let hits = {}
       for(let k of keysCount) {
+        let hitsToSort = response.hits.hits.filter( i => i._source[fieldName] && i._source[fieldName][0] == k.key ).map( h => h._source)
+        if(sort) {
+          hitsToSort = hitsToSort.sort((a,b)=>a[sort] && b[sort] && a[sort][0]*1 - b[sort][0]*1)
+          k.order = hitsToSort && hitsToSort[0][sort] && hitsToSort && hitsToSort[0][sort][0]*1
+        }
         hits[k.key] = { 
           count: k.doc_count,
-          hits: response.hits.hits.filter( i => i._source[fieldName] && i._source[fieldName][0] == k.key ).map( h => h._source)
+          hits: hitsToSort
         }
       }
       //Note that the above filter excludes hits that don't have the fieldName value, which we don't consider on the keyCount map below
 
+      if(sort) {
+        keysCount = keysCount.sort((a,b)=>a.order - b.order)
+      }
       return ({
         value: keysCount.map(e => e.key),
         href: encodeURI( response.resultsUrl ),
