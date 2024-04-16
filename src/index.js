58236faf78f3d719fd9912da8dae7633b13c3e80
dCastanho <dc@cob.pt>
2024-04-16 10:45:05 +0100

    Updated DashInfo signatures

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ import * as HttpRequest from "./HttpRequest.js"
 
 
 const dmEquipmentCount     = (query="*",                                    options={}) => new DashInfo(options, dmEquipCount, {query:query})
-const definitionCount      = (definitionName,   query="*",                  options={}) => new DashInfo(options, defCount, {query:query, definitionName:definitionName })
+const definitionCount      = (definitionName,   query="*",                  options={}) => new DashInfo(options, defCount, {query:query, definitionName:definitionName, tz: options.tz  })
 const domainCount          = (domainId,         query="*",                  options={}) => new DashInfo(options, domCount, {query:query, domainId:domainId })
-const fieldSum             = (defId, fieldName, query="*",                  options={}) => new DashInfo(options, fSum,     {query:query, defId:defId, fieldName:fieldName })
-const fieldAverage         = (defId, fieldName, query="*",                  options={}) => new DashInfo(options, fAverage, {query:query, defId:defId, fieldName:fieldName })
-const fieldValues          = (def,   fieldName, query="*", size=10,         options={}) => new DashInfo(options, fValues,  {query:query, def:def, fieldName:fieldName, size:size, sort:options.sort, ascending:options.ascending })
-const fieldWeightedAverage = (defId, fieldName, weightFieldName, query="*", options={}) => new DashInfo(options, fWeightedAverage, {query:query, defId:defId, fieldName:fieldName, weightFieldName:weightFieldName })
+const fieldSum             = (defId, fieldName, query="*",                  options={}) => new DashInfo(options, fSum,     {query:query, defId:defId, fieldName:fieldName, tz: options.tz })
+const fieldAverage         = (defId, fieldName, query="*",                  options={}) => new DashInfo(options, fAverage, {query:query, defId:defId, fieldName:fieldName, tz: options.tz })
+const fieldValues          = (def,   fieldName, query="*", size=10,         options={}) => new DashInfo(options, fValues,  {query:query, def:def, fieldName:fieldName, size:size, sort:options.sort, ascending:options.ascending, tz: options.tz })
+const fieldWeightedAverage = (defId, fieldName, weightFieldName, query="*", options={}) => new DashInfo(options, fWeightedAverage, {query:query, defId:defId, fieldName:fieldName, weightFieldName:weightFieldName, tz: options.tz  })
 const instancesList        = (definitionName,   query="*", size=10, start=0, sort="", ascending="", options={}) => new DashInfo(options, instList, {query:query, definitionName:definitionName, size:size, start:start, sort:sort, ascending:ascending })
 const httpGet              = (url, axiosConfig, options={}) => new DashInfo(options, HttpRequest.httpGet, {url, axiosConfig})
 const httpPost             = (url, data, axiosConfig, options={}) => new DashInfo(options, HttpRequest.httpPost, {url, data, axiosConfig})
